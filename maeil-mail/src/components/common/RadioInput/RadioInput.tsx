5751c953df80e067a5a0c34d2a6bbe564e82e60a
brgndy <brgndy96@gmail.com>
2024-08-28 02:12:41 +0900

    feat: 공통 라디오 인풋 컴포넌트 생성

---
+++ maeil-mail/src/components/common/RadioInput/RadioInput.tsx
@@ -0,0 +1,30 @@
+import { radioContainer, customRadio, hiddenRadio, checkIcon, selectText } from './radioInput.css';
+import CheckIcon from '@/assets/images/Check.svg?react';
+import { myStyle } from '@/styles/vars.css';
+import Input from '../Input/Input';
+import { InputHTMLAttributes } from 'react';
+
+interface RadioInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  category: string;
+  text: string;
+}
+
+export default function RadioInput({ category, text, ...props }: RadioInputProps) {
+  return (
+    <div className={radioContainer}>
+      <Input
+        variant="default"
+        type="radio"
+        id={`${category}`}
+        name="field"
+        value={`${category}`}
+        className={hiddenRadio}
+        {...props}
+      />
+      <label htmlFor={`${category}`} className={customRadio}>
+        <CheckIcon className={`${checkIcon} ${myStyle}`} />
+        <span className={selectText}>{text}</span>
+      </label>
+    </div>
+  );
+}
---
+++ maeil-mail/src/components/common/RadioInput/radioInput.css.ts
@@ -0,0 +1,60 @@
+import { style } from '@vanilla-extract/css';
+import { background1, borderColor1, text1 } from '@/styles/vars.css';
+
+export const radioContainer = style({
+  position: 'relative',
+  display: 'flex',
+  alignItems: 'center',
+  marginBottom: '2rem',
+});
+
+export const hiddenRadio = style({
+  display: 'none',
+});
+
+export const customRadio = style({
+  position: 'relative',
+  paddingLeft: '2rem',
+  cursor: 'pointer',
+  fontSize: '1.6rem',
+  userSelect: 'none',
+  '::before': {
+    content: '',
+    position: 'absolute',
+    left: '0',
+    top: '50%',
+    transform: 'translateY(-50%)',
+    width: '2rem',
+    height: '2rem',
+    border: `0.01rem solid ${borderColor1}`,
+    borderRadius: '0.2rem',
+    background: background1,
+  },
+  selectors: {
+    [`${hiddenRadio}:checked + &::before`]: {
+      backgroundColor: background1,
+      border: `0.01rem solid ${borderColor1}`,
+    },
+  },
+});
+
+export const checkIcon = style({
+  position: 'absolute',
+  stroke: text1,
+  left: '0.1rem',
+  top: '50%',
+  transform: 'translateY(-50%)',
+  display: 'none',
+  width: '2rem',
+  height: '2rem',
+  selectors: {
+    [`${hiddenRadio}:checked + ${customRadio} &`]: {
+      display: 'block',
+    },
+  },
+});
+
+export const selectText = style({
+  margin: '0 1rem',
+  fontSize: '1.8rem',
+});
