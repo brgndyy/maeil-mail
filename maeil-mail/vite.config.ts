38b9c33cd68bcbbb9369f7265dee3b2be7ccb7bb
brgndy <brgndy96@gmail.com>
2024-08-27 17:57:12 +0900

    chore: path alias 적용

--- maeil-mail/vite.config.ts
+++ maeil-mail/vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
 });
