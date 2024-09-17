468508de6ea090021ca6f8f9bb3eb72349db8145
brgndy <brgndy96@gmail.com>
2024-09-17 14:52:58 +0900

    refactor: ico 변경 및 메타태그 작성

--- maeil-mail/src/pages/AllQuestionPage/AllQuestionPage.tsx
+++ maeil-mail/src/pages/AllQuestionPage/AllQuestionPage.tsx
@@ -5,6 +5,7 @@ import ContentWrapper from '@/components/common/Wrapper/ContentWrapper';
 import QuestionList from '@/components/Question/QuestionList';
 import { CATEGORY } from '@/constants/category';
 import CategorySelect from '@/components/CategorySelect/CategorySelect';
+import MetaTag from '@/components/MetaTag/MetaTag';
 
 export default function AllQuestionPage() {
   const [category, setCategory] = useState<CategoryEN>('all');
@@ -16,6 +17,11 @@ export default function AllQuestionPage() {
 
   return (
     <ContentWrapper>
+      <MetaTag
+        title="매일 메일 모든 질문 페이지"
+        description="모든 질문을 담은 페이지"
+        url="/questions"
+      />
       <CategorySelect onClick={handleCategorySelect} category={category} />
       <QuestionList questionList={questionList} />
     </ContentWrapper>
--- maeil-mail/src/pages/Policy/PolicyPage.tsx
+++ maeil-mail/src/pages/Policy/PolicyPage.tsx
@@ -1,10 +1,16 @@
 import Tabs from '@/components/common/Tabs/Tabs';
 import TAB_LIST from '@/constants/tabInfo';
 import ContentWrapper from '@/components/common/Wrapper/ContentWrapper';
+import MetaTag from '@/components/MetaTag/MetaTag';
 
 export default function PolicyPage() {
   return (
     <ContentWrapper>
+      <MetaTag
+        title="매일메일 개인정보취급방침 "
+        description="개인정보취급방침 관련 내용을 담은 페이지"
+        url="/policy"
+      />
       <Tabs tabList={TAB_LIST}>
         <Tabs.ListWrapper>
           {TAB_LIST.map((tab, index) => (
