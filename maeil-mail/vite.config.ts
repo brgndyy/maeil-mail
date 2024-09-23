3efe7fc90a650a1ac4e5ade0744971fb68ec7595
brgndy <brgndy96@gmail.com>
2024-09-23 16:05:32 +0900

    fix: legacy 플러그인 설치 및 적용

--- maeil-mail/vite.config.ts
+++ maeil-mail/vite.config.ts
@@ -3,10 +3,18 @@ import react from '@vitejs/plugin-react';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import path from 'path';
 import svgr from 'vite-plugin-svgr';
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin(), svgr()],
+  plugins: [
+    react(),
+    vanillaExtractPlugin(),
+    svgr(),
+    legacy({
+      targets: ['defaults', 'not IE 11', 'safari >= 12'],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
